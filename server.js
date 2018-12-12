7d14cfb366786e5f5feb898b98f8ac42d07e445b
Lam Tran <lam.tran10@gmail.com>
2018-12-11 21:16:00 -0800

    modified server.js

--- server.js
+++ server.js
@@ -72,6 +72,12 @@ app.post('/recipes', jsonParser, (req, res) => {
   res.status(201).json(item);
 });
 
+app.delete('/recipes/:id', (req, res) => {
+  Recipes.delete(req.params.id);
+  console.log(`Deleted recipe item \`${req.params.id}\``);
+  res.status(204).end();
+});
+
 
 app.get('/recipes', (req, res) => {
   res.json(Recipes.get());
